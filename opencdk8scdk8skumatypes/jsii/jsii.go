36fe2e67294181ab6118399606d11061ea65f05f
Hunter-Thompson <aatman@auroville.org.in>
2021-12-01 08:54:06 +0000

    chore(release): v0.0.8

--- opencdk8scdk8skumatypes/jsii/jsii.go
+++ opencdk8scdk8skumatypes/jsii/jsii.go
@@ -5,16 +5,15 @@
 package jsii
 
 import (
-	_           "embed"
+	_          "embed"
 
-	_jsii_      "github.com/aws/jsii-runtime-go/runtime"
+	_jsii_     "github.com/aws/jsii-runtime-go/runtime"
 
-	constructs  "github.com/aws/constructs-go/constructs/v3/jsii"
-	cdk8s       "github.com/cdk8s-team/cdk8s-core-go/cdk8s/jsii"
-	cdk8splus17 "github.com/cdk8s-team/cdk8s-plus-17-go/cdk8splus17/jsii"
+	constructs "github.com/aws/constructs-go/constructs/v3/jsii"
+	cdk8s      "github.com/cdk8s-team/cdk8s-core-go/cdk8s/jsii"
 )
 
-//go:embed opencdk8s-cdk8s-kuma-types-0.0.7.tgz
+//go:embed opencdk8s-cdk8s-kuma-types-0.0.8.tgz
 var tarball []byte
 
 // Initialize loads the necessary packages in the @jsii/kernel to support the enclosing module.
@@ -22,9 +21,8 @@ var tarball []byte
 func Initialize() {
 	// Ensure all dependencies are initialized
 	cdk8s.Initialize()
-	cdk8splus17.Initialize()
 	constructs.Initialize()
 
 	// Load this library into the kernel
-	_jsii_.Load("@opencdk8s/cdk8s-kuma-types", "0.0.7", tarball)
+	_jsii_.Load("@opencdk8s/cdk8s-kuma-types", "0.0.8", tarball)
 }
